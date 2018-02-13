0ad40c28d3a94681782d50373fe264c9bf170fdf
bastinc <christophe.bastin@umons.ac.be>
2018-02-13 15:47:58 +0100

    update init

--- init_ardas.sh
+++ init_ardas.sh
@@ -4,10 +4,11 @@ touch ~/ardas/ardas/settings.py
 ln -s ~/ardas/ardas/settings.py settings
 touch ~/ardas/cronlog.log
 ln -s ~/ardas/cronlog.log cronlog
-mkdir -p ~/ardas/ardas/logs &>2 /dev/null
+mkdir -p ~/ardas/ardas/logs >> cronlog
 touch ~/ardas/ardas/logs/restart_msg.txt
 ln -s ~/ardas/ardas/logs/restart_msg.txt restart_msg
 touch ~/ardas/ardas/logs/msg_log
 ln -s ~/ardas/ardas/logs/msg_log msg_log
 touch ~/ardas/ardas/logs/data_log
 ln -s ~/ardas/ardas/logs/data_log data_log
+cat ~/ardas/instructions.txt
