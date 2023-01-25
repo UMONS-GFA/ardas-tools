9a99697fc8b079805ce6f4dc4d7d2fdd3c325373
Siméon <simeon.dekens@umons.ac.be>
2023-01-25 12:33:04 +0100

    Edits the upgrade and reset arduino script to add environment variables

--- reset_arduino_EEPROM.sh
+++ reset_arduino_EEPROM.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-cd ardas/arduino_reset
+echo "Setting environment variables"
+cd ardas/
+echo "export CMAKE_CURRENT_SOURCE_DIR=$pwd" >> ~/.bashrc
+
+cd arduino_reset/
+echo "export CMAKE_SOURCE_DIR=$pwd" > ~/.bashrc
+
+source ~/.bashrc
+
 rm -rf build
 mkdir build
 cd build
--- upgrade_sketch.sh
+++ upgrade_sketch.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-cd ardas/arduino_sketch
+echo "Setting environment variables"
+cd ardas/
+echo "export CMAKE_CURRENT_SOURCE_DIR=$pwd" >> ~/.bashrc
+
+cd arduino_sketch/
+echo "export CMAKE_SOURCE_DIR=$pwd" > ~/.bashrc
+
+source ~/.bashrc
+
+
 rm -rf build
 mkdir build
 cd build
